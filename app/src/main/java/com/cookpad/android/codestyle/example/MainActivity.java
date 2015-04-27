dcd3c402946e781c0450ffda545bc06fb060c11c
FUJI Goro (gfx) <goro-fuji@cookpad.com>
2015-04-27 14:00:07 +0900

    apply new style

--- app/src/main/java/com/cookpad/android/codestyle/example/MainActivity.java
+++ app/src/main/java/com/cookpad/android/codestyle/example/MainActivity.java
@@ -11,13 +11,13 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.cookpad.android.codestyle.R.layout.activity_main);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(com.cookpad.android.codestyle.R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -29,7 +29,7 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == com.cookpad.android.codestyle.R.id.action_settings) {
+        if (id == R.id.action_settings) {
             return true;
         }
 
