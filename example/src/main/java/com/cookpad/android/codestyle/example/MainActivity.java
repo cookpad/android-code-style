abed8a748ab58789a9f8fc6b6a8c43d73a9d0b01
FUJI Goro (gfx) <goro-fuji@cookpad.com>
2015-06-18 15:45:57 +0900

    tweaks

--- example/src/main/java/com/cookpad/android/codestyle/example/MainActivity.java
+++ example/src/main/java/com/cookpad/android/codestyle/example/MainActivity.java
@@ -1,7 +1,7 @@
 package com.cookpad.android.codestyle.example;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
