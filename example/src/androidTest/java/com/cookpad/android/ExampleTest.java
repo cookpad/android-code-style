da4f133aab345a88950a9875b45cc700a91aa292
FUJI Goro (gfx) <goro-fuji@cookpad.com>
2015-06-18 15:13:14 +0900

    allow org.hamcrest.MatcherAssert.*

---
+++ example/src/androidTest/java/com/cookpad/android/ExampleTest.java
@@ -0,0 +1,14 @@
+package com.cookpad.android;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+public class ExampleTest {
+
+    @Test
+    public void testExample() throws Exception {
+        assertThat(42, is(42));
+    }
+}
