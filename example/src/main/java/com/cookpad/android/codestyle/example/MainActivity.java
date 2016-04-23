aef2426f13655c76c467f9b71114274687080586
FUJI Goro (gfx) <gfuji@cpan.org>
2016-04-24 00:01:46 +0900

    add methods to example

--- example/src/main/java/com/cookpad/android/codestyle/example/MainActivity.java
+++ example/src/main/java/com/cookpad/android/codestyle/example/MainActivity.java
@@ -1,14 +1,47 @@
 package com.cookpad.android.codestyle.example;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    public MainActivity() {
+    }
+
+    public static Intent createIntent(Context context) {
+        return new Intent(context, MainActivity.class);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialize(this);
+    }
+
+    /**
+     * @param context
+     */
+    void initialize(Context context) {
+        // ...
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    void onClickButton(View view) {
+        // ...
     }
 }
