5c7a24e681be038d5024e31ec2ba3615bd4d9c4e
FUJI Goro (gfx) <gfuji@cpan.org>
2016-04-23 23:45:37 +0900

    enable INSERT_LINE_BREAK_AFTER_LAST_ATTRIBUTE and reformat code

--- example/src/main/java/com/cookpad/android/codestyle/example/MainActivity.java
+++ example/src/main/java/com/cookpad/android/codestyle/example/MainActivity.java
@@ -1,11 +1,7 @@
 package com.cookpad.android.codestyle.example;
 
 import android.os.Bundle;
-import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
